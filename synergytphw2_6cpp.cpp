5a2a4f29a3dc64f7818ec2d006652741b2cc7929
Iofakh <it.khramov@gmail.com>
2022-11-21 13:22:01 +0300

    pt_3 zodiac update.

--- synergytphw2_6cpp.cpp
+++ synergytphw2_6cpp.cpp
@@ -95,4 +95,90 @@ main()
 
             break;
     }
+
+    if (((month == 1)&&(day > 20))||((month == 2)&&(day < 20)))
+    {
+        printf("Your zodiac sign is a Aquarius.\n");
+    }
+    else
+    {
+        if (((month == 2)&&(day > 19))||((month == 3)&&(day < 21)))
+        {
+            printf("Your zodiac sign is a Pisces.\n");
+        }
+        else
+        {
+            if (((month == 3)&&(day > 20))||((month == 4)&&(day < 21)))
+            {
+                printf("Your zodiac sign is a Aries.\n");
+            }
+            else
+            {
+                if (((month == 4)&&(day > 20))||((month == 5)&&(day < 22)))
+                {
+                    printf("Your zodiac sign is a Taurus.\n");
+                }
+                else
+                {
+                    if (((month == 5)&&(day > 21))||((month == 6)&&(day < 22)))
+                    {
+                        printf("Your zodiac sign is a Gemini.\n");
+                    }
+                    else
+                    {
+                        if (((month == 6)&&(day > 21))||((month == 7)&&(day < 23)))
+                        {
+                            printf("Your zodiac sign is a Cancer.\n");
+                        }
+                        else
+                        {
+                            if (((month == 7)&&(day > 22))||((month == 8)&&(day < 22)))
+                            {
+                                printf("Your zodiac sign is a Leo.\n");
+                            }
+                            else
+                            {
+                                if (((month == 8)&&(day > 21))||((month == 9)&&(day < 24)))
+                                {
+                                    printf("Your zodiac sign is a Virgo.\n");
+                                }
+                                else
+                                {
+                                    if (((month == 9)&&(day > 23))||((month == 10)&&(day < 24)))
+                                    {
+                                        printf("Your zodiac sign is a Libra.\n");
+                                    }
+                                    else
+                                    {
+                                        if (((month == 10)&&(day > 23))||((month == 11)&&(day < 23)))
+                                        {
+                                            printf("Your zodiac sign is a Scorpio.\n");
+                                        }
+                                        else
+                                        {
+                                            if (((month == 11)&&(day > 22))||((month == 12)&&(day < 23)))
+                                            {
+                                                printf("Your zodiac sign is a Sagittarius.\n");
+                                            }
+                                            else
+                                            {
+                                                if (((month == 12)&&(day > 22))||((month == 1)&&(day < 21)))
+                                                {
+                                                    printf("Your zodiac sign is a Capricorn.\n");
+                                                }
+                                                else
+                                                {
+                                                    printf("Error in day or month");
+                                                }
+                                            }
+                                        }                                        
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
