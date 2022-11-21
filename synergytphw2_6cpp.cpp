33c955e08540142f1319c1ed96bbc060d52f213c
Iofakh <it.khramov@gmail.com>
2022-11-21 12:37:17 +0300

    pt_2 Chinese zodiac update

--- synergytphw2_6cpp.cpp
+++ synergytphw2_6cpp.cpp
@@ -20,4 +20,79 @@ main()
         printf("You weren't born on a leap year.\n");
     }
 
+    int еastern_calendar = year % 12;
+
+    switch (еastern_calendar)
+    {
+        case 0:
+            {
+                printf("Your Chinese zodiac sign is a monkey.\n");
+            }
+            break;
+        case 1:
+            {
+                printf("Your Chinese zodiac sign is a rooster.\n");
+            }
+            break;
+        case 2:
+            {
+                printf("Your Chinese zodiac sign is a dog.\n");
+            }
+
+            break;
+        case 3:
+            {
+                printf("Your Chinese zodiac sign is a pig.\n");
+            }
+
+            break;
+        case 4:
+            {
+                printf("Your Chinese zodiac sign is a rat.\n");
+            }
+
+            break;
+        case 5:
+            {
+                printf("Your Chinese zodiac sign is a ox.\n");
+            }
+
+                break;
+        case 6:
+            {
+                printf("Your Chinese zodiac sign is a tiger.\n");
+            }
+
+            break;
+        case 7:
+            {
+                printf("Your Chinese zodiac sign is a rabit.\n");
+            }
+
+            break;
+        case 8:
+            {
+                printf("Your Chinese zodiac sign is a dragon.\n");
+            }
+
+            break;
+        case 9:
+            {
+                printf("Your Chinese zodiac sign is a snake.\n");
+            }
+
+            break;
+        case 10:
+            {
+                printf("Your Chinese zodiac sign is a horse.\n");
+            }
+
+            break;
+        case 11:
+            {
+                printf("Your Chinese zodiac sign is a goat.\n");
+            }
+
+            break;
+    }
 }
